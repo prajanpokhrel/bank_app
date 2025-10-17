c0948d4c3de222b12b2b87eae320ccb59129429e
prajan pokhrel <prajanpokhrel09@gmail.com>
2025-10-17 21:56:50 +0545

    added a logo and conatiner in login screen

--- lib/features/login/presentation/views/login_screen.dart
+++ lib/features/login/presentation/views/login_screen.dart
@@ -5,6 +5,35 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            height: 140,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(25),
+                bottomRight: Radius.circular(25),
+              ),
+              color: Colors.amber,
+            ),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset("assets/images/logo-new.png", width: 200),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
