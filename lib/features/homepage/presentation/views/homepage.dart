5fdec24cef826b79ea594302f2d360b0c4dc0a31
prajanpokhrel <prajanpokhrel09@gmail.com>
2025-11-01 20:19:52 +0545

    added appbarsection in homepage

--- lib/features/homepage/presentation/views/homepage.dart
+++ lib/features/homepage/presentation/views/homepage.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
+
 
 class Homepage extends StatelessWidget {
   const Homepage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar());
+    return Scaffold(
+      body: Column(
+       
+        children: 
+      [
+        
+      Container(
+              height: 150,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(25),
+                  bottomRight: Radius.circular(25),
+                ),
+                color: Colors.amber,
+              ),
+              child: Row(
+              
+                children: [Padding(
+                  padding:  EdgeInsets.only(left: 2.h),
+                  child: Text("Good Morning \n uinova ", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+                ), Spacer(),Icon(Icons.notifications), SizedBox(width: 2.h,) ,Padding(
+                  padding:  EdgeInsets.only(right: 2.h),
+                  child: CircleAvatar(backgroundImage: AssetImage("assets/images/person.png"),),
+                )],)
+              ),
+              
+      ],),
+    );
   }
 }
